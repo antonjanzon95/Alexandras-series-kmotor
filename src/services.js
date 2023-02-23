deddc25c4f0079ca22a85911eded6d8c01cedf27
Mattias heurlin <mattias.heurlin@hotmail.com>
2023-02-23 15:00:25 +0100

    Base js for api

--- src/services.js
+++ src/services.js
@@ -1,7 +1,31 @@
-export async function getSeries(searchTerm) {
-    fetch('http://www.omdbapi.com/?apikey=2c325926&type=series&s=' + searchTerm)
+export async function getSeries(searchCategory) {
+
+    switch (searchCategory) { 
+
+        case 'RealityTv':
+            fetch('http://www.omdbapi.com/?apikey=2c325926&type=series&t=' + searchTerm)
+            .then((response) => response.json())
+            .then((data) => data);
+
+            break;
+        case 'Romans':
+            break;
+        case 'Action':
+            break;
+        
+
+        
+   const selectedMovie = fetch('http://www.omdbapi.com/?apikey=2c325926&type=series&t=' + searchTerm)
     .then((response) => response.json())
     .then((data) => data);
-  }
 
-  
+    const selectedMovieCatagory = selectedMovie.
+
+   fetch('http://www.omdbapi.com/?apikey=2c325926&type=series&s=' + searchTerm)
+    .then((response) => response.json())
+    .then((data) => data);
+
+
+
+  }
+}
