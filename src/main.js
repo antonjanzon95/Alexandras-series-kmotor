af4a737c02f2a1d93682b0ded9a723d104174ff6
nanidam <nani.dam@medieinstitutet.se>
2023-02-23 14:03:40 +0100

    render html of series

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import "./index.css";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-
-  </div>
+const seriesContainer = document.querySelector("#results");
+const createSerieContainer = document.createElement("div");
+createSerieContainer.innerHTML=`
+<img class="serie-poster" src="" alt="serie poster">
+<h2 class="serie-title"></h2>
+<p class="serie-year">year</p>
+<p class="serie-rating">rating</p>
+<p class="serie-genres">genres</p>
 `;
+seriesContainer.appendChild(createSerieContainer)
+
+console.log(createSerieContainer)
