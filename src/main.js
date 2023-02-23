e03d6299b24e493815267e914af269f919201d1a
nanidam <nani.dam@medieinstitutet.se>
2023-02-23 15:43:17 +0100

    added loop

--- src/main.js
+++ src/main.js
@@ -17,14 +17,17 @@ const createHtmlSeries = async () => {
   const series = await getSeries("married at first sight");
 
   const seriesContainer = document.querySelector("#results");
-  const createSerieContainer = document.createElement("div");
-  createSerieContainer.innerHTML = `
-  <img class="serie-poster" src="${series[0].Poster}" alt="serie poster">
-  <h2 class="serie-title">${series[0].Title}</h2>
-  <p class="serie-year">${series[0].Year}</p>
-  <p class="serie-rating">${series[0].Type}</p>
-  <p class="serie-genres">${series[0].Genre}</p>
-  `;
-  seriesContainer.appendChild(createSerieContainer);
+  for (let i=0; i<series.length; i++){
+    const createSerieContainer = document.createElement("div");
+    createSerieContainer.innerHTML = `
+    <img class="serie-poster" src="${series[i].Poster}" alt="serie poster">
+    <h2 class="serie-title">${series[i].Title}</h2>
+    <p class="serie-year">${series[i].Year}</p>
+    <p class="serie-rating">${series[i].Type}</p>
+    <p class="serie-genres">${series[i].Genre}</p>
+    `;
+    seriesContainer.appendChild(createSerieContainer);
+
+  }
 };
 createHtmlSeries();
