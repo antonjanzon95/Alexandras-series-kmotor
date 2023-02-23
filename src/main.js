1f5809f176758b8034025970e2ca9b45c7e320fe
nanidam <nani.dam@medieinstitutet.se>
2023-02-23 14:38:36 +0100

    render html from api

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import "./index.css";
+import {getSeries} from "./services"
 
-const seriesContainer = document.querySelector("#results");
-const createSerieContainer = document.createElement("div");
-createSerieContainer.innerHTML=`
-<img class="serie-poster" src="" alt="serie poster">
-<h2 class="serie-title"></h2>
-<p class="serie-year">year</p>
-<p class="serie-rating">rating</p>
-<p class="serie-genres">genres</p>
-`;
-seriesContainer.appendChild(createSerieContainer)
-
-console.log(createSerieContainer)
+const createHtmlSeries = async ()=>{
+  const series = await getSeries("married at first sight");
+  
+  const seriesContainer = document.querySelector("#results");
+  const createSerieContainer = document.createElement("div");
+  createSerieContainer.innerHTML=`
+  <img class="serie-poster" src="${series[0].Poster}" alt="serie poster">
+  <h2 class="serie-title">${series[0].Title}</h2>
+  <p class="serie-year">${series[0].Year}</p>
+  <p class="serie-rating">${series[0].Type}</p>
+  <p class="serie-genres">${series[0].Genre}</p>
+  `;
+  seriesContainer.appendChild(createSerieContainer)
+  }
+createHtmlSeries()
