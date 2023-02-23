863306ae979b736219846ec1f7b7cf61151dddfd
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-23 15:00:48 +0100

    Animations

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import "./index.css";
 
+const heading = document.querySelector("h1");
+const button = document.querySelector("button");
+const searchText = document.querySelector("#movieSearch");
 
-
-document.querySelector("#app").innerHTML = `
-  <div>
-
-  </div>
-`;
+gsap.to(heading, { y: "-20px", yoyo: true, repeat: -1, duration: 0.5 });
+gsap.fromTo(
+  button,
+  { x: "-100px" },
+  { x: "100px", yoyo: true, repeat: -1, duration: 0.3 }
+);
+gsap.to(searchText, { scale: 0.2, yoyo: true, repeat: -1, duration: 0.5 });
--- src/services.js
+++ src/services.js
@@ -1,7 +1,5 @@
 export async function getSeries(searchTerm) {
-    fetch('http://www.omdbapi.com/?apikey=2c325926&type=series&s=' + searchTerm)
+  fetch("http://www.omdbapi.com/?apikey=2c325926&type=series&s=" + searchTerm)
     .then((response) => response.json())
     .then((data) => data);
-  }
-
-  
+}
