4b4c61b8d322250ea5bc89947e8e762d043ee27e
Tim Sundell <tim920129@hotmail.com>
2023-02-24 09:14:42 +0100

    added api key as variable

--- src/services.js
+++ src/services.js
@@ -1,6 +1,7 @@
 export async function getSeries(searchTerm) {
+  const apiKey = "2c325926"
   return fetch(
-    "http://www.omdbapi.com/?apikey=2c325926&type=series&s=" + searchTerm
+    `http://www.omdbapi.com/?apikey=${apiKey}&type=series&s=` + searchTerm
   )
     .then((response) => response.json())
     .then((data) => data.Search);
